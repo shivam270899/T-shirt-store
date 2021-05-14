2780aa16097a20bee490597844461c3b5f6f599e
shivam270899 <68773657+shivam270899@users.noreply.github.com>
2021-05-14 13:54:24 +0530

    Add files via upload

---
+++ routes/category.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const router=express.Router();
+
+const {getCategoryById,createCategory,getCategory,getAllCategory,updateCategory,removeCategory}=require("../controllers/category");
+const {isSignedIn,isAdmin,isAuthenticated}=require("../controllers/auth");
+const {getUserById}=require("../controllers/user");
+
+//PARAMS
+router.param("userId",getUserById);
+router.param("categoryId",getCategoryById);
+
+//ACTUAL ROUTES
+
+//create
+router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin,createCategory);
+
+//read
+router.get("/category/:categoryId",getCategory);
+router.get("/categories",getAllCategory);
+
+//upadate
+router.put("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,updateCategory);
+
+//delete
+router.delete("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,removeCategory);
+
+
+module.exports=router;
